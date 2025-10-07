b54545b7067d04f46e113128ba3aec28ce95c85b
Alexis <godwillaguemonbg@gmail.com>
2025-10-07 09:09:11 +0100

    Ajout de l'exercie de l'Algorithme d'Euclide en C

---
+++ 📁 C-Prep-Projects/03-Test_primalité/primalité.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+void is_premier(int a){
+    int b = sqrt(a);
+    int compteur = 0;
+    for (int i = 1 ; i<= b; i++){
+        if ( ( a/i) == 0)
+            compteur++;
+    }
+    if
+}
+
+int main(void){
+
+
+
+    return 0;
+}
