ad96b0b8df9942ebd02c4367f5fd0e2ab71b1b6a
Alexis <godwillaguemonbg@gmail.com>
2025-10-07 10:16:45 +0100

    Ajout de l'exercie du test de la primalité  en C

--- 📁 C-Prep-Projects/03-Test_primalité/primalité.c
+++ 📁 C-Prep-Projects/03-Test_primalité/primalité.c
@@ -2,17 +2,22 @@
 #include <math.h>
 
 void is_premier(int a){
+
     int b = sqrt(a);
     int compteur = 0;
-    for (int i = 1 ; i<= b; i++){
-        if ( ( a/i) == 0)
-            compteur++;
-    }
-    if
+
+    for (int i = 2 ; i<= b; i++){
+        
+        if ( a%b == 0) compteur++;
+        }
+        
+    if( compteur == 0 && a > 1)
+        printf("Le nombre est premier\n");
+    else 
+        printf("Le nombre n'est pas premier\n");
 }
 
 int main(void){
-
 
 
     return 0;
